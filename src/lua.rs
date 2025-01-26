9195df1ea12fa1823aece7d3f964a392d5a03e19
Arbiter Waldorff <53384860+rbwldrf@users.noreply.github.com>
2025-01-26 21:14:43 +0000

    Update lua.rs

--- src/lua.rs
+++ src/lua.rs
@@ -73,17 +73,17 @@ impl<'gc> Context<'gc> {
     }
 
     // Calls `ctx.globals().get(key)`
-    pub fn get_global<V: FromValue<'gc>>(self, key: &str) -> Result<V, TypeError> {
+    pub fn get_global<V: FromValue<'gc>>(self, key: &'static str) -> Result<V, TypeError> {
         self.state.globals.get(self, key)
     }
 
     // Calls `ctx.globals().get_value(key)`
-    pub fn get_global_value(self, key: &str) -> Value<'gc> {
+    pub fn get_global_value(self, key: &'static str) -> Value<'gc> {
         self.state.globals.get_value(self, key)
     }
 
     // Calls `ctx.globals().set_field(key, value)`
-    pub fn set_global<V: IntoValue<'gc>>(self, key: &str, value: V) -> Value<'gc> {
+    pub fn set_global<V: IntoValue<'gc>>(self, key: &'static str, value: V) -> Value<'gc> {
         self.state.globals.set_field(self, key, value)
     }
 
